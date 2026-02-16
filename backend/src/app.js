9f5eca3030cda919627d160a6cba6dcf6a87c82e
Dubzy <70661510+Dubzy08@users.noreply.github.com>
2026-02-16 09:06:47 -0500

    configured user creation api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const signupRoute = require('./routes/signup');
+const bodyParser = require('body-parser');
 
 const app = express();
-
 const PORT = 3000 || process.env.PORT;
+
+app.use(bodyParser.json());
+
+app.use('/user', signupRoute);
 
 app.listen(PORT, () => {
     console.log(`Server running on: http://localhost:${PORT}`)
