830d300dd1bfafde9a56e41464159a5bc7f84105
Dubzy <70661510+Dubzy08@users.noreply.github.com>
2026-02-27 09:24:11 -0500

    Server naming modification

---
+++ server/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const signupRoute = require('./src/routes/signup');
+const loginRoute = require('./src/routes/login');
+const userRoute = require('./src/routes/user');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const createAdminAccount = require('./src/scripts/admin');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+createAdminAccount();
+
+let connections = 0;
+
+io.on('connection', socket =>{
+    socket.emit('message', 'Welcome to weMessage!');
+    
+    // Broadcast when user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // Run when client disconnects
+    socket.on('disconnect', () =>{
+        io.emit('message', 'User left the chat')
+    })
+
+    // Listen for chat message
+    socket.on('chatMessage', (message) =>{
+        io.emit('message', message);
+    })
+});
+
+app.use('/user', signupRoute);
+app.use('/auth', loginRoute);
+app.use('/api', userRoute);
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => {
+    console.log(`Server running on: http://localhost:${PORT}`)
+});
